bdd9e60411dfad52b67b9f2510be8cdd7e5b6898
Johanderson Mogollon <johander1822@gmail.com>
2022-12-02 14:16:43 +0100

    try again

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PubSubx.MixProject do
     [
       app: :pub_subx,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
