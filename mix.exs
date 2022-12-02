86e6fc5b82623f448c0152ae766c68ec995ee1f5
Johanderson Mogollon <johander1822@gmail.com>
2022-12-02 14:39:16 +0100

    publish stuffs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule PubSubx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+
+      description: "A lightweigh PubSub implementation",
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,8 +24,18 @@ defmodule PubSubx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["sonic182"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/sonic182/pub_subx"},
+      files: ~w(lib mix.exs README.md .formatter.exs)
     ]
   end
 end
