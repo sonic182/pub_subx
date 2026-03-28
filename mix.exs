42c8025287499d6c7995b6b0513ecf729b328769
sonic182 <johander1822@gmail.com>
2026-03-28 13:28:00 +0100

    fix credo

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule PubSubx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
